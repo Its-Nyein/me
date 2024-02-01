46536711e123ffd73b20673d404a3078ffb9f6a5
Nyein Phyo Aung <nyeinphyoaung.edu@gmail.com>
2024-02-02 02:12:35 +0630

    update for the PrductData

--- data/projectData.js
+++ data/projectData.js
@@ -4,8 +4,8 @@ export const projects = [
         description: "A game store e-commerce project with incorporating dynamic features.",
         stacks: ["React.js", "Bootstrap",],
         image: "/game_store.png",
-        sourceCode: "https://github.com/Its-Nyein/tenzies",
-        liveAt: "https://github.com",
+        sourceCode: "https://github.com/Its-Nyein/game-shop",
+        liveAt: "https://gameshopie.vercel.app/",
     },
 
     {
@@ -13,7 +13,7 @@ export const projects = [
         description: "A dynamic and no responsive movie website project. Animated carousel slider, dynamic filter and animated header with scroll effect.",
         stacks: ["React.js", "Bootstrap",],
         image: "/movie_website.png",
-        sourceCode: "https://github.com/Its-Nyein/note-app",
+        sourceCode: "https://github.com/Its-Nyein/movie-website",
         liveAt: "https://github.com",
     },
 
@@ -23,8 +23,8 @@ export const projects = [
         description: "Animated modern E-commerce website.",
         stacks: ["React.js", "Tailwind CSS",],
         image: "/shopify.png",
-        sourceCode: "https://github.com/Its-Nyein/note-app",
-        liveAt: "https://github.com",
+        sourceCode: "https://github.com/Its-Nyein/shopify",
+        liveAt: "https://eshopify.vercel.app/",
     },
 
     {
@@ -32,8 +32,8 @@ export const projects = [
         description: "A fun to play mini game project consists of various dice represented in terms of numerical values.",
         stacks: ["React.js", "Vite",],
         image: "/tenzies.png",
-        sourceCode: "https://github.com/Its-Nyein/note-app",
-        liveAt: "https://github.com",
+        sourceCode: "https://github.com/Its-Nyein/tenzies",
+        liveAt: "https://tenzies-five-livid.vercel.app/",
     },
 
     {
@@ -42,7 +42,7 @@ export const projects = [
         stacks: ["React.js", "Firebase", "Vite"],
         image: "/note_app.png",
         sourceCode: "https://github.com/Its-Nyein/note-app",
-        liveAt: "https://github.com",
+        liveAt: "https://notiesapp.vercel.app/",
     },
 
     {
@@ -50,8 +50,8 @@ export const projects = [
         description: "A simple no responsive weather app.",
         stacks: ["React.js", "Vite", "Tailwind"],
         image: "/weather_app.png",
-        sourceCode: "https://github.com/Its-Nyein/note-app",
-        liveAt: "https://github.com",
+        sourceCode: "https://github.com/Its-Nyein/weather-app",
+        liveAt: "https://github.com/Its-Nyein/weather-app",
     },
 
 ]
